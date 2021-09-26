cc248375fc43ab4cd5439dd3437a2d51cd52a5a5
Yanick <tyankee15@gmail.com>
2021-09-26 05:00:17 -0300

    Update meta data in public/index.html

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
-const SignUp = () => (
-  <div>
-    <h1>Welcome to signup page</h1>
-  </div>
-);
+const SignUp = () => {
+  const [errors, setErrors] = useState([]);
+  const [msg, setMsg] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const runLoginAuth = async (email, password) => {
+
+  }
+
+  const handleSubmit = (email, password) => {
+    runLoginAuth(email, password);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(email, password);
+  };
+
+  return (
+    <div>
+      <h1 className="heading">Login</h1>
+      <div className="content">
+        <div>
+          <form onSubmit={onSubmit} className="form">
+            <div className="form__group">
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form__group">
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form__group">
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {/* <button type="submit" className="btn mb3">{btnName}</button> */}
+            <button type="submit" className="btn mb3">Save</button>
+          </form>
+        </div>
+        <Link to="/" className="btn">Go back to Home</Link>
+        <h3>
+          Already registered, <span><Link to="/login" className="btn">Login Here</Link></span>
+        </h3>
+      </div>
+    </div>
+  )
+}
+
+
 
 export default SignUp;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Login from '../components/Login';
 import SignUp from '../components/SignUp';
+import Dashboard from '../components/Dashboard';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const AppRouter = () => (
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={SignUp} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </div>
     </div>
