f7c0f088382848b2f68a00ff28b7566548afbdaf
Yanick <tyankee15@gmail.com>
2021-09-26 08:57:03 -0300

    Refactor login and register pages and add fuctions to autheticate and sumbit data to backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,62 +1,97 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { API_BASE_URL } from '../helpers/api';
+import { withRouter } from "react-router-dom";
 
-const Login = () => {
-  const [errors, setErrors] = useState([]);
-  const [msg, setMsg] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const runLoginAuth = async (email, password) => {
-
+function Login(props) {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    successMessage: null
+  })
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setState(prevState => ({
+      ...prevState,
+      [id]: value
+    }))
   }
 
-  const handleSubmit = (email, password) => {
-    runLoginAuth(email, password);
-  };
-
-  const onSubmit = (e) => {
+  const handleSubmitClick = (e) => {
     e.preventDefault();
-    handleSubmit(email, password);
-  };
-
+    const payload = {
+      "email": state.email,
+      "password": state.password,
+    }
+    axios.post(API_BASE_URL + 'dashboard', payload)
+      .then(function (response) {
+        if (response.data.code === 200) {
+          setState(prevState => ({
+            ...prevState,
+            'successMessage': 'Login successful. Redirecting to Dashboard..'
+          }))
+          redirectToHome();
+          props.showError(null)
+        }
+        else if (response.data.code === 204) {
+          props.showError("Username and password do not match");
+        }
+        else {
+          props.showError("Username does not exists");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  const redirectToHome = () => {
+    props.updateTitle('Home')
+    props.history.push('/');
+  }
+  const redirectToRegister = () => {
+    props.history.push('/register');
+    props.updateTitle('Register');
+  }
   return (
-    <div>
-      <h1 className="heading">Login</h1>
-      <div className="content">
-        <div>
-          <form onSubmit={onSubmit} className="form">
-            <div className="form__group">
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="form__group">
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            {/* <button type="submit" className="btn mb3">{btnName}</button> */}
-            <button type="submit" className="btn mb3">Save</button>
-          </form>
+    <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+      <form>
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+            value={state.email}
+            onChange={handleChange}
+          />
         </div>
-        <Link to="/" className="btn">Go back to Home</Link>
-        <h3>
-          Not registered, <span><Link to="/register" className="btn">Signup Here</Link></span>
-        </h3>
+        <div className="form-group text-left">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={state.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-check">
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmitClick}
+        >Submit</button>
+      </form>
+      <div className="alert alert-success mt-2" style={{ display: state.successMessage ? 'block' : 'none' }} role="alert">
+        {state.successMessage}
+      </div>
+      <div className="registerMessage">
+        <span>Dont have an account? </span>
+        <span className="loginText" onClick={() => redirectToRegister()}>Register</span>
       </div>
     </div>
   )
 }
 
-
-
-export default Login;
+export default withRouter(Login);
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,72 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { API_BASE_URL, ACCESS_TOKEN_NAME } from '../helpers/api';
+import { withRouter } from 'react-router-dom';
 
 function SignUp(props) {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    successMessage: null
+  })
+
+  const sendDetailsToServer = () => {
+    if (state.email.length && state.password.length) {
+      props.showError(null);
+      const payload = {
+        "email": state.email,
+        "password": state.password,
+      }
+      axios.post(API_BASE_URL + '/user/register', payload)
+        .then(function (response) {
+          if (response.status === 200) {
+            setState(prevState => ({
+              ...prevState,
+              'successMessage': 'Registration successful. Redirecting to Home page...'
+            }))
+            localStorage.setItem(ACCESS_TOKEN_NAME, response.data.token);
+            redirectToHome();
+            props.showError(null)
+          } else {
+            props.showError("Some error occurred")
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      props.showError('Please enter valid username and password')
+    }
+  }
+
+  const redirectToHome = () => {
+    props.updateTitle('Home')
+    props.history.push('/')
+  }
+
+  const redirectToLogin = () => {
+    props.updateTitle('Login')
+    props.history.push('/login')
+  }
+
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setState(prevState => ({
+      ...prevState,
+      [id]: value
+    }))
+  }
+
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if (state.password === state.confirmPassword) {
+      sendDetailsToServer()
+    } else {
+      props.showError('Passwords do not match')
+    }
+  }
+
   return (
     <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
       <form>
@@ -10,6 +76,8 @@ function SignUp(props) {
             className="form-control"
             id="email"
             placeholder="Enter email"
+            value={state.email}
+            onChange={handleChange}
           />
         </div>
         <div className="form-group text-left">
@@ -18,6 +86,8 @@ function SignUp(props) {
             className="form-control"
             id="password"
             placeholder="Password"
+            value={state.password}
+            onChange={handleChange}
           />
         </div>
         <div className="form-group text-left">
@@ -26,17 +96,27 @@ function SignUp(props) {
             className="form-control"
             id="confirmPassword"
             placeholder="Retype Password"
+            value={state.confirmPassword}
+            onChange={handleChange}
           />
         </div>
         <button
           type="submit"
           className="btn btn-primary"
+          onClick={handleSubmitClick}
         >
           Register
         </button>
       </form>
+      <div className="alert alert-success mt-2" style={{ display: state.successMessage ? 'block' : 'none' }} role="alert">
+        {state.successMessage}
+      </div>
+      <div className="mt-2">
+        <span>Already have an account? </span>
+        <span className="loginText" onClick={() => redirectToLogin()}>Login here</span>
+      </div>
     </div>
   )
 }
 
-export default SignUp;
+export default withRouter(SignUp);
